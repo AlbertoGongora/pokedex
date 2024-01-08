524ee3518f9db47c342c75b006088f678bf8aaac
Pabloortegagil <pabloortega@gmail.com>
2024-01-08 22:36:56 +0100

    modificación js y html

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const browser = document.querySelector('#browser');
 const pokeCard = document.querySelector('#pokecard')
 const pokeName = document.querySelector('#poke-name')
 const pokeImgContainer = document.querySelector('#poke-img-container')
@@ -7,22 +8,55 @@ const pokeHeightWeight = document.querySelector('#poke-height-weight')
 const pokeTypes = document.querySelector('#poke-types')
 const pokeStats = document.querySelector('#poke-stats')
 
-const busquedaPokemon = event => { 
+const busquedaPokemon = (event) => {
     event.preventDefault();
-    const {nombrePokemon} = event.target.browser
-    fetch(`http://pokeapi.co/api/v2/pokemon/${nombrePokemon.toLowerCase()}`)
-        .then(data => data.json())
-        .then(response => informacionPokemon(response))
-} 
-
-
-const informacionPokemon = data => { 
-    const sprite = data.sprite.front_default;
-    const {stats, types} = data;
-    console.log(data);
+    const { value } = event.target.browser;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('No se encontró el Pokémon');
+            }
+            return response.json();
+        })
+        .then(informacionPokemon)
+        .catch(error => {
+            console.error(error);
+            mostrarError();
+        });
 }
 
-console.log(informacionPokemon);
+const mostrarError = () => {
+    pokeName.textContent = 'Pokémon no encontrado, inténtalo de nuevo';
+    pokeImg.src = './assets/img/pokeshadow.png'; // Ruta a la imagen de fallo
+    pokeId.textContent = '';
+    pokeHeightWeight.textContent = '';
+    pokeTypes.innerHTML = '';
+    pokeStats.innerHTML = '';
+}
+
+const informacionPokemon = data => {
+    const spritesFront = data.sprites.other["official-artwork"].front_default;
+    const {name, id, height, weight, types, stats} = data;
+
+    pokeName.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+    pokeImg.src = spritesFront;
+    pokeId.textContent = `ID: #${id}`;
+    pokeHeightWeight.textContent = `Height: ${height} in | Weight: ${weight} lb`;
+
+    pokeTypes.innerHTML = '';
+    types.forEach(type => {
+        const typeElement = document.createElement('div');
+        typeElement.textContent = type.type.name;
+        pokeTypes.appendChild(typeElement);
+    });
+
+    pokeStats.innerHTML = '';
+    stats.forEach(stat => {
+        const statElement = document.createElement('div');
+        statElement.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+        pokeStats.appendChild(statElement);
+    });
+}
 
 
 
