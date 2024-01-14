bab12073961386b3e298ec76df5d55939a09ee87
Alberto Góngora <gongorabada@gmail.com>
2024-01-14 18:12:09 +0100

    Modificaciones en la funcionalidad, y añadidos comentarios en JavaScript

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,24 @@
-import { informacionPokemon } from "./pokedex_main.js";
+'use strict';
 
-const maxPokemons = 1012; // Cantidad máxima de pokemones
+// Importaciones0
+import { informacionPokemon, setCurrentPokemonID } from "./pokedex_main.js";
+
+// Variables
+const maxPokemons = 1010; // Cantidad máxima de pokemones Oficiales a dia de hoy
 const listContent = document.querySelector('.list-content'); // Lista de pokemones  
 const browser = document.querySelector('#browser-search-input'); // Buscador
 const closeButton = document.querySelector(".browser-close");
 const pokemonNoEncontradoTexto = document.querySelector('.mensajeError'); // Pokemon no encontrado
 const pokemonNoEncontradoImg = document.querySelector('.img-mensajeError'); // Pokemon no encontrado
 
-
 // Matriz de pokemones 
 let allPokemons = []; // Array de pokemones
-let selectedPokemonID = 1; // ID del pokemon seleccionado y que pasara esa id al otro archivo para mostrar la informacion
+let selectedPokemonID = 1;
 
+// Carga inicial de pokemones
 const localStoragePokemonList = JSON.parse(localStorage.getItem('allPokemons'));
 
+// Verifica si hay pokemones guardados en el localStorage, si no los hay, obtiene los pokemones
 if (localStoragePokemonList) {
     allPokemons = localStoragePokemonList;
     visualizarPokemons(allPokemons);
@@ -28,41 +33,50 @@ if (localStoragePokemonList) {
         });
 }
 
-
-
 // Visualiza los pokemones
 function visualizarPokemons(pokemon) {
     listContent.innerHTML = '';
 
+    // Visualiza los pokemones
     pokemon.forEach((pokemon) => {
-            const pokemonID = pokemon.url.split('/')[6];
-    const listObject = document.createElement('ul');
-            listObject.className = "list-object";
-            listObject.innerHTML = `
-                    <li class="numero-pokemon">
-                        <p class="seleccionID">#${pokemonID}</p> 
-                    </li>
-                    <li class="img-pokemon">
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${pokemon.name}"> 
-                    </li>
-                    <li class="nombre-pokemon">
-                        <p class="titulo-fonts">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p> 
-                    </li>
-            `;           
-            listContent.appendChild(listObject);
+        // Obtiene el ID del pokemon
+        const pokemonID = pokemon.url.split('/')[6];
 
+        // Crea el elemento
+        const listObject = document.createElement('ul');
+        listObject.className = "list-object";
+
+        // Crea el HTML
+        listObject.innerHTML = `
+            <li class="numero-pokemon">
+                <p class="seleccionID">#${pokemonID}</p> 
+            </li>
+            <li class="img-pokemon">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${pokemon.name}"> 
+            </li>
+            <li class="nombre-pokemon">
+                <p class="titulo-fonts">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p> 
+            </li>
+        `;   
+
+        // Agrega el elemento
+        listContent.appendChild(listObject);
     });
 }
 
+// Evento para que se actualice la lista de pokemones cada vez que introducis un valor
 browser.addEventListener('keyup', controlBusquedaInput);
 
 // Controla la búsqueda cuando introduces un valor
 function controlBusquedaInput() {
+    // Obtiene el valor del input
     const busquedaElemento = browser.value.toLowerCase();
     let filtradoDePokemons;
 
+    // Verifica si es un número
     const siEsNumero = /^\d+$/.test(busquedaElemento);
 
+    // Filtra los pokemones
     if (siEsNumero) {
         filtradoDePokemons = allPokemons.filter((pokemon) => {
             const pokemonID = pokemon.url.split('/')[6];
@@ -76,8 +90,10 @@ function controlBusquedaInput() {
         });
     }
 
+    // Visualiza los pokemones
     visualizarPokemons(filtradoDePokemons);
 
+    // Verifica si hay pokemones
     if (filtradoDePokemons.length === 0) {
         pokemonNoEncontradoTexto.style.display = 'block';
         pokemonNoEncontradoImg.style.display = 'block';
@@ -87,49 +103,78 @@ function controlBusquedaInput() {
     }
 }
 
+// Evento click para limpiar el buscador
 closeButton.addEventListener("click", clearSearch);
 
+// Limpia el buscador
 function clearSearch() {
-  browser.value = "";
-  visualizarPokemons(allPokemons);
-  pokemonNoEncontradoTexto.style.display = 'none';
-  pokemonNoEncontradoImg.style.display = 'none';
+    // Limpia el input
+    browser.value = "";
+    // Llama a la función para visualizar los pokemones
+    visualizarPokemons(allPokemons);
+    // Desactiva la visualización de pokemones no encontrados
+    pokemonNoEncontradoTexto.style.display = 'none';
+    pokemonNoEncontradoImg.style.display = 'none';
 }
 
 // Abrir el modal
-function abrirModal(pokemonID) {
+function abrirModal(selectedPokemonID) {
+    // Mostrar el modal
     const modal = document.getElementById('pokedex-modal');
     modal.style.display = 'flex';
 
     // Llamar a la función informacionPokemon con el ID del Pokémon seleccionado
-    informacionPokemon(pokemonID);
+    informacionPokemon(selectedPokemonID);
 }
-
 
 // cerrar el modal
-function cerrarModal() {
+function cerrarModal(event) {
+    // Obtener el modal
     const modal = document.getElementById('pokedex-modal');
-    modal.style.display = 'none';
+    
+    // Cerrar el modal con el boton cerrar o al hacer click afuera
+    if (
+        !event.target.closest('.pokedex-modal-int') ||
+        event.target.classList.contains('close')
+    ) {
+        modal.style.display = 'none';
+
+        // Limpia el valor de currentPokemonID dandole un null para que el algoritmo de cambiarPokemon y seleccionar  funcione
+        setCurrentPokemonID(null);
+    }
 }
-
-
 
 // Actualiza el evento click en cada list-object para abrir el modal
 listContent.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Obtener el elemento
     const target = event.target.closest('.list-object');
+
+    // Verifica que se haya seleccionado
     if (target) {
         // Obtener el ID del Pokémon al hacer clic
         const numeroPokemonElement = target.querySelector('.seleccionID');
         if (numeroPokemonElement) {
-            selectedPokemonID = numeroPokemonElement.textContent.slice(1);
+            // Obtener el ID
+            const numeroID = numeroPokemonElement.textContent.slice(1);
+
+            // Convierte el ID a un numero
+            selectedPokemonID = parseInt(numeroID);
+
+            // Llama a la función abrirModal
             abrirModal(selectedPokemonID);
         } else {
+            // Manejo de errores
             console.error('Elemento numero-pokemon no encontrado.');
         }
     }
 });
 
+// Evento click para cerrar el modal, por el boton y por el fondo
 document.querySelector('.close').addEventListener('click', cerrarModal);
+document.querySelector('.pokedex-modal-ext').addEventListener('click', cerrarModal);
 
-export { selectedPokemonID};
+// Exportar las variables
+export { selectedPokemonID, maxPokemons, cerrarModal };
 
--- JS/pokedex_main.js
+++ JS/pokedex_main.js
@@ -1,31 +1,37 @@
-import { selectedPokemonID } from './main.js';
+'use strict';
 
+// Importaciones
+import { selectedPokemonID, maxPokemons, cerrarModal } from './main.js';
+
+// Variables para la gestion de que pokemon se muestra
 const pokeCard = document.querySelector('#pokecard');
+let currentPokemonID = null;
 
-
+// Colores
 const typeColors = {
-    electric: '#F8D030',
-    normal: '#A8A878',
+    electric: '#eed535',
+    normal: '#a4acaf',
     fire: '#fd7d24',
-    water: '#6890F0',
-    ice: '#98D8D8',
-    rock: '#B8A038',
+    water: '#4592c4',
+    ice: '#51c4e7',
+    rock: '#a38c21',
     flying: '#A890F0',
     grass: '#9bcc50',
     psychic: '#f366b9',
-    ghost: '#705898',
-    bug: '#A8B820',
+    ghost: '#7b62a3',
+    bug: '#729f3f',
     poison: '#b97fc9',
-    ground: '#E0C068',
-    dragon: '#7038F8',
-    steel: '#B8B8D0',
-    fighting: '#C03028',
-    dark: '#705848',
-    fairy: '#EE99AC',
+    ground: '#ab9842',
+    dragon: '#f16e57',
+    steel: '#9eb7b8',
+    fighting: '#d56723',
+    dark: '#7b62a3',
+    fairy: '#fdb9e9',
     unknown: '#68A090',
     shadow: '#000000'
 };
 
+// Traducciones de los tipos de los pokemons
 const typeTranslations = {
     electric: 'eléctrico',
     normal: 'normal',
@@ -49,6 +55,7 @@ const typeTranslations = {
     shadow: 'sombra'
 };
 
+// Traducciones de las estadísticas
 const statsTranslations = {
     hp: 'Vida',
     attack: 'Ataque',
@@ -58,55 +65,74 @@ const statsTranslations = {
     speed: 'Velocidad'
 }
 
-function informacionPokemon() {
-    // Usar el ID del Pokémon directamente desde la variable seleccionada
-    const pokemonID = selectedPokemonID;
+// Llamada a la API para obtener los detalles del pokemon
+async function informacionPokemon() {
+    try {
 
-    // Realizar una segunda solicitud para obtener detalles específicos del Pokémon
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}/`)
-        .then((response) => response.json())
-        .then(pokemonDetails => {
-            // Verificar si la solicitud fue exitosa
-            if (!pokemonDetails || pokemonDetails.detail) {
-                console.error('No se pudo obtener la información detallada del Pokémon.');
-                return;
-            }
-            mostrarPokedex({ pokemonDetails });
-        })
-        .catch(error => {
-            console.error('Error al obtener detalles del Pokémon:', error);
-        });
+        const pokemonID = currentPokemonID === null ? selectedPokemonID : currentPokemonID;
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemonID || pokemonID}/`);
+        const pokemonDetails = await response.json();
+
+        if (!pokemonDetails || pokemonDetails.detail) {
+            console.error('No se pudo obtener la información detallada del Pokémon.');
+            return;
+        }
+
+        mostrarPokedex({ pokemonDetails });
+    } catch (error) {
+        console.error('Error al obtener detalles del Pokémon:', error);
+    }
 }
 
+// Funcion para sumar o restar el ID y asi mostrar el pokemon correspondiente en la navegacion
+function cambiarPokemon(operacion) {
+    const numeroIDActual = currentPokemonID === null ? selectedPokemonID : currentPokemonID;
+    currentPokemonID = numeroIDActual;
+    currentPokemonID += operacion;
 
+    informacionPokemon();
+}
+
+// Funcion para mostrar la informacion del pokemon
 function mostrarPokedex({ pokemonDetails}) {
 
+    // Destructurar el objeto pokemonDetails
     const { name, id, sprites, height, weight, types, stats } = pokemonDetails;
     
+    // Crear elementos del DOM
+    //Imagen de fondo
     const imgFondoPokeball = document.createElement('img');
     
+    //Nombre del Pokemon
     const pokeName = document.createElement('li');
 
+    //Contenerdor con la imagen del Pokemon y las flechas para poder navegar entre pokemones
     const pokeImgContainer = document.createElement('li');
     const pokeImg = document.createElement('img');
     const flechaderecha = document.createElement('img');
     const flechaizquierda = document.createElement('img');
     
+    //Carta donde estan los datos de los pokemon
     const cardForm = document.createElement('ul');
     
+    //Contenedor con ID y boton de cambio de imagen
     const pokemonImgShinyContainer = document.createElement('ul');
     const shinyButton = document.createElement('button');
     const pokeId = document.createElement('li');
 
+    //Contenedor con los datos de altura y peso
     const pokeHeightWeight = document.createElement('li');
 
+    //Contenedor con los tipos y estadisticas, asociados a unas funciones
     const pokeTypes = PokemonTypes(types);
     const pokeStats = PokemonStats(stats);
 
-            
+    // Imagenes de los pokemones
     const spritesFront = sprites.other['official-artwork'].front_default;
     const pokemonShiny = sprites.other['official-artwork'].front_shiny;
 
+    // Asociar clases a los elementos
     imgFondoPokeball.classList.add('img-pokeball-background');
     pokeName.classList.add('poke-name');
     pokeImgContainer.classList.add('poke-img-container');
@@ -121,18 +147,28 @@ function mostrarPokedex({ pokemonDetails}) {
     pokeStats.classList.add('poke-stats');
     cardForm.classList.add('card-form');
 
+    // Variables para ajustar el peso y altura de los pokemones de los datos de la PokeAPI
     const centimetros = 10;
     const kilogramos = 10;
 
+    // Asociar atributos a los elementos
     imgFondoPokeball.src = './assets/img/pokeballBackground.svg';
     pokeName.textContent = name.charAt(0).toUpperCase() + name.slice(1);
     flechaizquierda.src = './assets/img/chevron_left.svg';
     flechaderecha.src = './assets/img/chevron_right.svg';
-    pokeImg.src = spritesFront;
+
+    // Logica para asignar imagen y si no tiene mostrar otra imagen por defecto
+    if (spritesFront === null) {
+        pokeImg.src = './assets/img/imagenNoDisponiblePikaPika.png'
+    } else {
+        pokeImg.src = spritesFront;
+    }
+    
     pokeId.textContent = `ID: #${id}`;
     shinyButton.textContent = 'Ver shiny';
     pokeHeightWeight.textContent = `Altura: ${height * centimetros} cm | Peso: ${weight / kilogramos} kg`;
 
+    // Agregar elementos al DOM
     pokeCard.innerHTML = '';
     pokeCard.appendChild(imgFondoPokeball);
     pokeCard.appendChild(pokeName);
@@ -150,38 +186,55 @@ function mostrarPokedex({ pokemonDetails}) {
     cardForm.appendChild(pokeTypes);
     cardForm.appendChild(pokeStats);
 
+    // DIFERENTES FUNCIONALIDADES DE LA POKEDEX
+
+    // Cambiar imagen shiny con el botón y volver a la imagen normal
     let shinyOffOn = false;
 
+    // Funcion para cambiar la imagen de shiny
     function toggleShiny() {
+        // Logica para asignar imagen y si no tiene mostrar otra imagen por defecto
         if (shinyOffOn) {
-            pokeImg.src = spritesFront;
-            
+            if (spritesFront === null) {
+                pokeImg.src = './assets/img/imagenNoDisponiblePikaPika.png'
+            } else {
+                pokeImg.src = spritesFront;
+            }
         } else {
-            pokeImg.src = pokemonShiny;
+            if (pokemonShiny === null) {
+                pokeImg.src = './assets/img/imagenNoDisponiblePikaPika.png'
+            } else {
+                pokeImg.src = pokemonShiny;
+            }
         }
+        // Cambiar el estado de shiny al ejecutarlo
         shinyOffOn = !shinyOffOn;
 
-        // Cambiar el color de fondo del botón
+    // Cambiar el estilo del boton al ejecutarlo
     shinyButton.style.backgroundColor = shinyOffOn ? '#fa607a' : '#dc0a2d';
     shinyButton.style.color = shinyOffOn ? 'black' : 'white';
     }
 
+    // Agregar evento al boton de cambio de imagen
     shinyButton.addEventListener('click', toggleShiny);
 
+    // Activar transicion de imagen, agranda cuando estamos encima y disminuye cuando estamos fuera de ella
     pokeImg.addEventListener('mouseover', encimaDeImg);
     pokeImg.addEventListener('mouseout', fueraDeImg);
 
+    // Activar transicion de imagen, agranda cuando estamos encima 
     function encimaDeImg() {
         pokeImg.style.transition = 'transform 0.4s ease-in';
         pokeImg.style.transform = 'scale(1.2)';
     }
     
+    // Activar transicion de imagen, disminuye cuando estamos fuera de ella
     function fueraDeImg() {
         pokeImg.style.transition = 'transform 0.4s ease-in';
         pokeImg.style.transform = 'scale(1)';
     }
 
-    //cambiar color barra depende de los tipos
+    //cambiar color barra depende de los tipos, cogiendo por defecto el segundo tipo, si no hay segundo tipo se toma el primero
     const type = types[1] || types[0];
     const barInner = document.querySelectorAll('.bar-inner');
 
@@ -189,71 +242,103 @@ function mostrarPokedex({ pokemonDetails}) {
         barInner.style.backgroundColor = typeColors[type.type.name];
     });
 
+    //Al estar al inicio y al final de la pokedex, no muestra las flechas hacia donde no quedan Pokemons
+    if (id ===1) {
+        // Si estas en el numero uno no muestra la flecha izquierda
+        flechaizquierda.style.display = 'none';
+        }
+    
+    // Si estas en el numero maximo no muestra la flecha derecha
+        if (id === maxPokemons) {
+            flechaderecha.style.display = 'none';
+    }
+
+    // Eventos para navegar por la pokedex
+    flechaizquierda.addEventListener('click', () => cambiarPokemon(-1));
+    flechaderecha.addEventListener('click', () => cambiarPokemon(1));
 }
 
+// Funcion para obtener los tipos y que se tengan el estilo segun el color correspondiente al tipo
 const PokemonTypes = (types) => {
+    // Crea contenedor para los tipos y asocia una clase
     const typesContainer = document.createElement('ul');  
     typesContainer.classList.add('poke-types');
 
+    // Crea un elemento para cada tipo
     types.forEach(type => {
+        // Crea un elemento li y asocia una clase para cada tipo
         const typeElement = document.createElement('li'); 
         typeElement.classList.add('type');
+
+        // Asigna el color correspondiente al tipo
         typeElement.style.backgroundColor = typeColors[type.type.name];
 
+        // Variables que determinan el color que se le asigna al tipo
         const firstType = types[0].type.name;
         const typeName = type.type.name;
 
+        // Asigna el color de fondo del primer tipo de ese pokemon
         pokeCard.style.backgroundColor = typeColors[firstType];
 
+        // Traduce el nombre del tipo con la función typeTranslations
         const translatedTypeName = typeTranslations[typeName] || typeName;
 
+        // Asigna el texto del tipo traducido
         typeElement.textContent = translatedTypeName.charAt(0).toUpperCase() + translatedTypeName.slice(1);
 
         // Agregar el elemento al contenedor pokeTypes
         typesContainer.appendChild(typeElement);
     });
 
+    // Devuelve el contenedor de los tipos
     return typesContainer;
 }
 
+// Funcion para obtener los estadisticas
 const PokemonStats = (stats) => {
-    const statsContainer = document.createElement('div');
+    // Crea contenedor para las estadisticas
+    const statsContainer = document.createElement('menu');
     statsContainer.classList.add('poke-stats'); 
 
+    // Crea un elemento para cada estadistica
     stats.forEach(stat => {
-        const statElement = document.createElement('menu');
+        // Crea un elemento li y asocia una clase para cada estadistica
+        const statElement = document.createElement('ul');
         const statName = stat.stat.name;
         let valorEstado = stat.base_stat;
 
+        // Asigna el valor minimo de la estadistica, para que no se pase de 250
         valorEstado = Math.min(valorEstado, 250);
 
+        // Traduce el nombre de la estadistica con la función statsTranslations
         const statNameTranslated = statsTranslations[statName] || statName;
 
+        // Asigna el texto de la estadistica traducido
         statElement.innerHTML = `
         <ul class="stat-row">
-        <li class="stat-desc">${statNameTranslated.charAt(0).toUpperCase() + statNameTranslated.slice(1)}</li>
+            <li class="stat-desc">${statNameTranslated.charAt(0).toUpperCase() + statNameTranslated.slice(1)}</li>
         <ul class="stat-bar">
             <li class="bar-inner" style="width: ${valorEstado / 250 * 100}%"></li>
         </ul>
-        <li class="stat-number">${valorEstado}</li>
+            <li class="stat-number">${valorEstado}</li>
         </ul>
         `;
 
         // Agregar el elemento al contenedor pokeStats
         statsContainer.appendChild(statElement);
-            
-
     });
 
+    // Devuelve el contenedor de las estadisticas
     return statsContainer;
 }
 
-export { informacionPokemon }
+// Funcion para que cuando se cierre el modal el valor currentPokemonID sea null, exportado a main.js
+function setCurrentPokemonID(value) {
+    currentPokemonID = value;
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-    const pokeCard = document.querySelector('#pokecard');
-    
-    informacionPokemon(pokeCard); 
-});
+// Exportar las funciones
+export { informacionPokemon, currentPokemonID, setCurrentPokemonID };
+
 
 
